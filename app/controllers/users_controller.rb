3883348f0c34ad3fba800bd4e9eda39c820481ff
omitea8 <omitea8@gmail.com>
2023-01-24 21:25:38 +0900

    loginボタンの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,7 @@
 class UsersController < ApplicationController
 
-    def index
-        users = User.all
-        render json: { data: users }
+    def login
+        render json: { url: 'http://google.com' }
     end
-
-    def create
-        user = User.new(user_params)
-        if user.save
-            render json: user, status: :created
-        else
-            render json: user.errors, status: :unprocessable_entity
-        end
-    end
-
-    private
-
-    def user_params
-        params.except(:user).permit(:uid)
-      end
-    
 
 end
