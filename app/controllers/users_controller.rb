13083aac94176e902b84482c450ed6fdf79de845
omitea8 <omitea8@gmail.com>
2023-01-25 17:06:04 +0900

    twitterへのリダイレクトURLの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
 
     def login
-        render json: { url: 'http://google.com' }
+        render json: { url: 'https://twitter.com/i/oauth2/authorize?'\
+            'response_type='\
+            'code&client_id=YWZ0cFpwWGVsSGlVcVgwTGJ6elo6MTpjaQ'\
+            '&redirect_uri=http://127.0.0.1:3000/auth/twitter/callback'\
+            '&scope=users.read'\
+            '&state=abc'\
+            '&code_challenge=E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM'\
+            '&code_challenge_method=s256' }
     end
 
 end
