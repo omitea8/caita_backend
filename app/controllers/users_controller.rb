fe20c49f5f0bc91a58fecab98b90ac603a6c145d
omitea8 <omitea8@gmail.com>
2023-01-26 17:59:59 +0900

    リクエストURL作成のためのハッシュ化とセッション保存を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
 
+    include ActionController::Cookies
+
     def login
+        state = SecureRandom.hex(16)
+        session[:state] = state
+        challengeOrigin = SecureRandom.hex(16)
+        session[:challengeOrigin] =challengeOrigin
+        challenge = Digest::SHA256.hexdigest(challengeOrigin)
         render json: { url: 'https://twitter.com/i/oauth2/authorize?'\
             'response_type='\
             'code&client_id=YWZ0cFpwWGVsSGlVcVgwTGJ6elo6MTpjaQ'\
             '&redirect_uri=http://127.0.0.1:3000/auth/twitter/callback'\
             '&scope=users.read'\
-            '&state=abc'\
-            '&code_challenge=E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM'\
+            "&state=#{state}"\
+            "&code_challenge=#{challenge}"\
             '&code_challenge_method=s256' }
     end
 
