17768257efe658c6e35b5f82aac3014a4179b553
omitea8 <omitea8@gmail.com>
2023-02-08 11:31:28 +0900

    フロントエンドにログイン可否を送る・プルリク修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,37 +13,47 @@ class UsersController < ApplicationController
         challengeHash = Digest::SHA256.digest(challengeVerifier)
         challenge = Base64.urlsafe_encode64(challengeHash, padding: false)
         render json: { url: 'https://twitter.com/i/oauth2/authorize?'\
-            'response_type='\
-            'code&client_id=YWZ0cFpwWGVsSGlVcVgwTGJ6elo6MTpjaQ'\
-            '&redirect_uri=http://localhost:3000/auth/twitter/callback'\
-            '&scope=users.read'\
+            "response_type=code"\
+            "&client_id=#{ENV['CLIENT_ID']}"\
+            "&redirect_uri=#{ENV['TWITTER_CALLBACK_URL']}"\
+            "&scope=users.read"\
             "&state=#{state}"\
             "&code_challenge=#{challenge}"\
-            '&code_challenge_method=S256' }
+            '&code_challenge_method=S256'}
     end
 
     def getToken 
         # stateの検証
         checkstate = params[:state] === session[:state]
-        # リクエストトークンの作成
+        # stateの検証がtrueだったら
         if checkstate == true
+            # リクエストトークンの作成
             url = URI.parse('https://api.twitter.com/2/oauth2/token')
             req = Net::HTTP::Post.new(url.path)
             req.basic_auth ENV['CLIENT_ID'], ENV['CLIENT_SECRET']
             req.set_form_data({
                 'grant_type'=>'authorization_code',
-                'client_id'=>'YWZ0cFpwWGVsSGlVcVgwTGJ6elo6MTpjaQ',
+                'client_id'=>ENV['CLIENT_ID'],
                 'code'=>params[:code],
                 'code_verifier'=>session[:challengeVerifier],
-                'redirect_uri'=>'http://localhost:3000/auth/twitter/callback'
+                'redirect_uri'=>ENV['TWITTER_CALLBACK_URL']
             })
             http = Net::HTTP.new(url.host, 443)
             http.use_ssl = true
             res = http.start {|http| http.request(req) }
+            # 返ってきたbodyをJSONに変換
             json = JSON.parse(res.body)
+            # アクセストークンをsessionに保存
             session[:accessToken] = json['access_token']
+            # HTTPステータスメッセージをフロントエンドに送るためにJSON化
+            message = { message: res.message }
+            messageJson = message.to_json
+            render json: messageJson
+            puts messageJson
         else
+        # stateの検証がfalseだったら 
         puts checkstate
+
         end
     end
 
