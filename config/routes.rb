7b39786b79dc5294e025c8cd5537e8d55383496d
omitea8 <omitea8@gmail.com>
2023-02-25 15:13:46 +0900

    rubocopを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'creators/login'
   post 'creators/gettoken'
   get 'creators/getprofile'
+  get 'creators/imagedata'
   get 'creators/:creatorID', to:'creators#creator'
-  get 'creatros/imagedata'
 
 end
