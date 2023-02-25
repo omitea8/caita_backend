146a3ed4e22f10248951fa36011aafce37150bfb
omitea8 <omitea8@gmail.com>
2023-02-25 15:11:54 +0900

    デモデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 
 30.times do
-    DemoDatum.create!(
-        title: Faker::Lorem.sentence(word_count: 10),
-        caption: Faker::Lorem.sentence(word_count: 50),
+    Image.create!(
+        title: Faker::Lorem.sentence(word_count: 3),
+        caption: Faker::Lorem.sentence(word_count: 10),
         image_url: "https://picsum.photos/300",
         creator_id: 1,
         created_at: Faker::Date.between(from: 1.year.ago, to: Date.today)
