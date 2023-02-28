aba1b1e20413fb14f09a3930570cc9e6d0450b87
omitea8 <omitea8@gmail.com>
2023-02-28 16:00:00 +0900

    creatorIDを使ってイラスト一覧を取得

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,8 @@
 class ImagesController < ApplicationController
   def imagedata
-    render json: Image.where(creator_id: '1').to_json
+    creator = Creator.find_by(twitter_id: params[:creatorID])
+    creatorId = Creator.find_by(id: creator)
+    senddata = Image.where(creator_id: creatorId)
+    render json: senddata.to_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'creators/login'
   post 'creators/gettoken'
   get 'creators/getprofile'
-  get 'images/imagedata'
   get 'creators/:creatorID', to: 'creators#creator'
+  get 'images/:creatorID', to: 'images#imagedata'
 end
