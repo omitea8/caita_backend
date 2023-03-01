74faf7ad958ca6ef04a80b8b45e7ee2d31686307
omitea8 <omitea8@gmail.com>
2023-03-01 12:46:05 +0900

    フロントエンドに送る情報を必要なものだけにする

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,7 @@ class ImagesController < ApplicationController
   def imagedata
     creator = Creator.find_by(twitter_id: params[:creatorID])
     creatorId = Creator.find_by(id: creator)
-    senddata = Image.where(creator_id: creatorId)
+    senddata = Image.where(creator_id: creatorId).select(:title, :caption, :image_url, :id)
     render json: senddata.to_json
   end
 end
