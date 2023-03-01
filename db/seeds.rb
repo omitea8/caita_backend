e24bc2978d194999b713b3f495d7653937cfb54c
omitea8 <omitea8@gmail.com>
2023-03-01 12:46:38 +0900

    動作確認のための2人のユーザーのデモイラストを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
-
 30.times do
-    Image.create!(
-        title: Faker::Lorem.sentence(word_count: 3),
-        caption: Faker::Lorem.sentence(word_count: 10),
-        image_url: "https://picsum.photos/300",
-        creator_id: 1,
-        created_at: Faker::Date.between(from: 1.year.ago, to: Date.today)
-    )
+  Image.create!(
+    title: Faker::Lorem.sentence(word_count: 3),
+    caption: Faker::Lorem.sentence(word_count: 10),
+    image_url: 'https://picsum.photos/300',
+    creator_id: 2,
+    created_at: Faker::Date.between(from: 1.year.ago, to: Date.today)
+  )
 end
