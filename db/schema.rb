26907cbbc55ebf46d9be97c8fc8a6fae851d0864
omitea8 <omitea8@gmail.com>
2023-03-02 15:30:21 +0900

    ユーザー作成とユーザー1・2のイラストデータを作成するように変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_24_065345) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_24_061801) do
   create_table "creators", force: :cascade do |t|
     t.string "twitter_system_id"
     t.string "twitter_id"
