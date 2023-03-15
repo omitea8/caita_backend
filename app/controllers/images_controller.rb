2bfb43ff74083d8b0066cae30af97d01d517a0f3
omitea8 <omitea8@gmail.com>
2023-03-15 14:58:58 +0900

    titleカラムを削除

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,14 +1,13 @@
 class ImagesController < ApplicationController
   def imagelist
     creator = Creator.find_by(twitter_id: params[:creatorID])
-    senddata = Image.where(creator_id: creator.id).select(:title, :caption, :image_url, :id)
+    senddata = Image.where(creator_id: creator.id).select(:caption, :image_url, :id)
     render json: senddata.to_json
   end
 
   def imagedata
     image = Image.find_by(id: params[:imageID])
     senddata = {
-      title: image.title,
       caption: image.caption,
       image_url: image.image_url,
       created_at: image.created_at
