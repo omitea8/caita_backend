771dc1fa327d8ad362ed33c62ee45f19df877b7a
omitea8 <omitea8@gmail.com>
2023-06-20 13:09:58 +0900

    image_nameカラムの追加
    storage_urlカラムは必要なかったため削除しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_035633) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_040819) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -57,8 +57,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_035633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "storage_name"
+    t.string "image_name"
     t.index ["creator_id", "created_at"], name: "index_images_on_creator_id_and_created_at"
     t.index ["creator_id"], name: "index_images_on_creator_id"
+    t.index ["image_name"], name: "index_images_on_image_name", unique: true
     t.index ["storage_name"], name: "index_images_on_storage_name", unique: true
   end
 
