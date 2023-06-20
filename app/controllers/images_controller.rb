4d037b2106d0c261390e14fb422e6350c5852c8a
omitea8 <omitea8@gmail.com>
2023-06-20 13:55:15 +0900

    image_nameに自動生成した文字列の保存を追加

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -35,6 +35,7 @@ class ImagesController < ApplicationController
       render json: 'NG'.to_json
       return
     end
+    create_image_name(image)
     storage_name = create_storage_name(image)
     upload_to_aws(params[:image], storage_name)
     image_url = "https://#{ENV.fetch('AWS_BUCKET')}.s3.#{ENV.fetch('AWS_REGION')}.amazonaws.com/#{storage_name}"
@@ -78,7 +79,6 @@ class ImagesController < ApplicationController
 
   private
 
-  # ランダムな文字列を生成
   def generate_random_string
     SecureRandom.urlsafe_base64(12)
   end
@@ -96,19 +96,31 @@ class ImagesController < ApplicationController
     storage_name
   end
 
+  def create_image_name(image)
+    result = false
+    image_name = generate_random_string
+    while result == false
+      begin
+        result = image.update(image_name: image_name)
+      rescue StandardError
+        image_name = generate_random_string
+      end
+    end
+    image_name
+  end
+
   # 画像のデータを作成
   def create_image_from(params)
     Image.new(caption: params[:caption], creator_id: @current_creator.id)
   end
 
-  # 画像のバリデーション
+  # バリデーション
   def validate_image(image)
     image.is_a?(ActionDispatch::Http::UploadedFile) &&
       ['image/png', 'image/gif', 'image/jpeg'].include?(image.content_type) &&
       image.size <= 20.megabytes
   end
 
-  # キャプションのバリデーション
   def validate_caption(caption)
     caption.is_a?(String) && caption.length <= 1000
   end
